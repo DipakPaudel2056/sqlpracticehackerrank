ced6d0a723b2ce258378e4aa2b44159e18a04890
dipakpaudel2056 <dipakpaudel2056@gmail.com>
2025-04-05 19:55:18 +1100

    calculated euclidean distance

--- manhattandistance.sql
+++ manhattandistance.sql
@@ -6,3 +6,8 @@
 --  happens to equal the maximum value in Western Longitude (LONG_W in STATION).
 -- Query the Manhattan Distance between points  and  and round it to a scale of  decimal places.
 SELECT  ROUND((MAX(LAT_N) -MIN(LAT_N)) + (MAX(LONG_W) - MIN(LONG_W)),4) FROM STATION; 
+
+
+-- EUCLIDEAN DISTANCE
+
+SELECT ROUND(SQRT(POWER((MIN(LAT_N) - MAX(LAT_N)),2) + POWER((MIN(LONG_W) - MAX(LONG_W)),2)),4) FROM STATION;
